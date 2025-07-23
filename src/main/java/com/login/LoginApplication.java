bab97b598f752b999b5dcdf4c31ad31375c20df0
황정빈 <hjbin1211@gmail.com>
2025-07-23 14:56:59 +0900

    User

--- src/main/java/com/login/LoginApplication.java
+++ src/main/java/com/login/LoginApplication.java
@@ -1,4 +1,4 @@
-package com.login.login;
+package com.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/login/domain/User.java
@@ -0,0 +1,25 @@
+package com.login.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
